5a9413a85a9608e4274d5017fafb39c95405eb28
KylerHansen <Kyler4Hansen@gmail.com>
2025-02-04 17:47:58 -0700

    title update

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Coming Soon</h1>
 
       <nav>
         <Link to={Routes.HOME}>Home</Link>
