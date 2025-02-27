3e0be59ca51c72a9d5d265de3f82af1caefc83a6
KylerHansen <Kyler4Hansen@gmail.com>
2025-02-26 18:25:27 -0700

    add express server and connect to aws lambda

--- packages/app/backend/src/server.ts
+++ packages/app/backend/src/server.ts
@@ -1,18 +1,41 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
-/* import { HttpStatusCode } from "@portfolio/common-universal/src/http-status-code"; */
+import cors, { CorsOptions } from "cors";
+import serverless from "serverless-http";
+
+const allowedOrigin = {
+  development: ["http://localhost:5173/portfolio/"],
+  production: ["https://kylerhansen.github.io/portfolio/"],
+};
+
+export enum HttpStatusCode {
+  Ok = 200,
+  BadRequest = 400,
+  GenericServerError = 500,
+}
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT;
+
+const env =
+  process.env.NODE_ENV === "development" ? "development" : "production";
+
+const corsOptions: CorsOptions = {
+  methods: ["GET", "POST"],
+  origin: allowedOrigin[env] || [],
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
 app.get("/health", (req: Request, res: Response) => {
-  res.sendStatus(200);
+  res.sendStatus(HttpStatusCode.Ok);
 });
 
-app.listen(port, () => {
-  console.log(`Backend server is running on http://localhost:${port}`);
+app.post("/askJarvis", (req: Request, res: Response) => {
+  res.sendStatus(HttpStatusCode.Ok);
 });
+
+export const handler = serverless(app);
