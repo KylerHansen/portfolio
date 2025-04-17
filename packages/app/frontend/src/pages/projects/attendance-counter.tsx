e95378a3103076503848f7112e4ac1d839a90330
KylerHansen <Kyler4Hansen@gmail.com>
2025-04-17 14:31:24 -0600

    update attendance project details page content

--- packages/app/frontend/src/pages/projects/attendance-counter.tsx
+++ packages/app/frontend/src/pages/projects/attendance-counter.tsx
@@ -5,21 +5,10 @@ export function AttendanceCounter() {
     <div>
       <ProjectDetails
         title="Attendance Counter"
-        objective="To create a counting app for counting and reporting attendance."
-        requirements={[
-          "Must have a click counter.",
-          "Must be able to store and organize each count.",
-          "Must be able to graph saved counts over a period of time.",
-        ]}
-        description="
-        Inspiration for this project came as I asked my local church clerk how I could make him a better click counter. 
-        He gave me some ideas for how I could make something better than his current click counter.
-        I created a click counter that was easy to count attendance with. 
-        You could set the numeric intervals in which you wanted to increment or decrement by. 
-        I used room database to store the counts locally. I made it so counts could be organized into books or a collection of counts.
-        It was also easy to add historic data into the books. You could generate graphs or download .csv files to share the data with others via SMS or gmail.
-        "
-        stack="Java, Android studio"
+        situation="Inspiration for this project came as I saw my local church clerk using an app to count ward attendance. I asked him how I could make him a better one. 
+        He gave me some ideas for things he would like to see in a click counter."
+        action="I created a click counter that was easier for him to count attendance with. You could easily set the numeric intervals in which you wanted to increment or decrement by. Each of the weekly attendance counts could be saved in various files such as 'Ward Attendance', 'Sunday School Attendance', etc. The user could select one or more of these folders to use in generating a graph. The graph and data could be shared via text, gmail, or csv."
+        tools={["Java", "Android studio", "Eclipse", "RoomDB"]}
         imageUrls={["attendance.png"]}
         result="I completed all the requirements and the minimum viable product (MVP) for this was completed. I did not publish this on the store because my church ended up releasing a feature in their app specific for clerk use that had internal reporting built into it. Effectively making this project obsolete."
       ></ProjectDetails>
